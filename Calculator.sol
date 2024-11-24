71e94f2eaf4e8bda14ab8b87e125c8bcdee22f44
rangelcho1900@gmail.com <^1#S0Sp$f$EX>
2024-11-24 19:36:45 +0200

    init work

---
+++ Calculator.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+contract Calculator {
+    function calculateCompoundInterest(uint256 principal, uint256 rat, uint256 year) external  {
+    }
+}
