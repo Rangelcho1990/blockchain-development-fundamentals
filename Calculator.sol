f0189d9ad17d2afbe3bab74e1eba0a8cfd872804
Rangelcho1990 <rangelcho1900@gmail.com>
2024-11-24 21:19:41 +0200

    add Log Error example and Compound Interest contracts

--- Calculator.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.28;
-
-contract Calculator {
-    function calculateCompoundInterest(uint256 principal, uint256 rat, uint256 year) external  {
-    }
-}
