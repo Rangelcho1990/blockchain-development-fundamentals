506c34a7b80a4404f8d70f4499d17fa8c1e7962a
Rangelcho1990 <rangelcho1900@gmail.com>
2024-12-04 18:30:11 +0200

    add new work

--- Solidity Fundamentals/Lab/Reference.sol
+++ Solidity Fundamentals/Lab/Reference.sol
@@ -2,33 +2,39 @@
 
 pragma solidity 0.8.28;
 
-contract Crowdfunding {
-    mapping(address => uint256) public shares;
-
-    function addShares(address receiver) external {
-        shares[receiver] += 100;
-    }
+struct Vote{
+    address shareholder;
+    uint256 shares;
+    uint256 timestamp;
 }
 
-contract Test {
-    uint256[5] public arr = [5,3];
+contract Crowdfunding {
+    mapping(address => uint256) public shares;
+    Vote[] public votes;
 
-    
-    uint256[] public dynamicArr = [1, 2, 3, 4, 5];
+    uint256 public sharePrice = 1 ether;
 
-    function removeArr() external returns (uint256[] memory) {
-        dynamicArr.pop();
+    uint256 public hours5 = 5 hours;
+    uint256 public min = 5 minutes;
+    uint256 public sec = 5 seconds;
 
-        return dynamicArr;
+    // constructor(uint256 _initSharePrice){
+    //     sharePrice = _initSharePrice;
+    // }
+
+    function addShares() external payable {
+        // shares[receiver] += 100;
+        // shares[msg.sender] += 1000; // we know who calls the method, cannot be manipuleted
+        shares[msg.sender] += msg.value; // works with payble
     }
 
-    function addNumber() external view returns (uint256) {
-        uint256 res;
-
-        for (uint256 i = 0; i < arr.length; i++) {
-            res += arr[i];
-        }
-
-        return res;
+    function vote(address holder) external {
+        votes.push(
+            Vote({
+                shareholder: holder,
+                shares: shares[holder],
+                timestamp: block.timestamp
+            })
+        );
     }
 }
